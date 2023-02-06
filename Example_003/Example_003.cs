38c5067a415194cd71cabbdca4f7797f14950207
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-06 21:17:54 +0300

    Написали программу по вычеслению произведений ряда чисел

---
+++ Example_003/Example_003.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int Multi(int number)
+{
+    int prod = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        prod *= i;
+    }
+    return prod;
+}
+
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {Multi(N)}");
