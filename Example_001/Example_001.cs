5ddd3e9beaae7f4703e802677ec2d91d96882243
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-06 20:34:01 +0300

    Программа с функцией по нахождению ряда суммы чисел

---
+++ Example_001/Example_001.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int GetSum(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+         sum += i;
+    }
+
+    return sum;
+
+}
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма чисел от 1 до {N} равна {GetSum(N)}");
