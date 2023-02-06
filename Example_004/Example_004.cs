82bc0398ae21ed91d90576ae57a093d24ca0ed9d
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-06 21:45:58 +0300

    Вывели массив, состоящей из нулей и единиц

---
+++ Example_004/Example_004.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+int[] GetArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void printArray(int[] inArray)
+
+{
+    for (int i = 0; i < inArray.Length; i++)
+
+      Console.Write($"{inArray[i]} ");
+}
+
+int[] array = GetArray();
+printArray(array);
